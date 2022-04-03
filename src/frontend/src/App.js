94324492731680b85d1e27f5acaedceb86cd63ca
jcorderop <iorch.cordero@gmail.com>
2022-04-04 00:16:00 +0200

    added andt design - add unfetch for rest request - display in table backend data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,53 @@
-import {Button} from "antd";
+import {Button, Table} from "antd";
+
 import './App.css';
 
+import {getAllUsers} from "./RestClient";
+import {useEffect, useState} from "react";
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'UserType',
+        dataIndex: 'userType',
+        key: 'userType',
+    },
+];
+
 function App() {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = () => {
+        getAllUsers()
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data);
+            });
+    }
+
+    useEffect( () => {
+        console.log("component is mounted");
+        fetchUsers();
+    }, []);
+
   return (
     <div className="App">
-      <Button type="primary">login</Button>
+        <Button type="primary">login</Button>
+        <Table dataSource={users} columns={columns} />
     </div>
   );
 }
