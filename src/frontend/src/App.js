859939eb428b698ffe1e2516738840117f1126e3
jcorderop <iorch.cordero@gmail.com>
2022-04-03 23:17:11 +0200

    added frontend project and andt design

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from "antd";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">login</Button>
+    </div>
+  );
+}
+
+export default App;
