1b28f9f933229d4c3f4504c1848484f24d8e3cd3
jcorderop <iorch.cordero@gmail.com>
2022-04-30 23:42:41 +0200

    added - test passed again

--- src/test/java/com/cordero/fullstack/FullstackApplicationTests.java
+++ src/test/java/com/cordero/fullstack/FullstackApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cordero.fullstack;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +9,7 @@ class FullstackApplicationTests {
 	@Test
 	void contextLoads() {
 		//issuer solved
-		Assertions.fail("oops test will fail again....");
+		//Assertions.fail("oops test will fail again....");
 	}
 
 }
